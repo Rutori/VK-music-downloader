ebc13506a260275f2d76b9b69c6005c1d5efc969
Rutori <mishin_neo@bk.ru>
2016-12-11 14:13:55 +0300

    Add files via upload

---
+++ musicvk/musicvk/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Никитка
+ * Date: 30.07.2016
+ * Time: 13:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace musicvk
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.PathBox = new System.Windows.Forms.TextBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.progressLabel = new System.Windows.Forms.Label();
+			this.TokenBox = new System.Windows.Forms.TextBox();
+			this.GetTokenButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(75, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(200, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Вставьте токен с доступом к аудио";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(50, 52);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(55, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Токен:";
+			// 
+			// PathBox
+			// 
+			this.PathBox.Location = new System.Drawing.Point(120, 85);
+			this.PathBox.Name = "PathBox";
+			this.PathBox.Size = new System.Drawing.Size(106, 20);
+			this.PathBox.TabIndex = 6;
+			this.PathBox.Click += new System.EventHandler(this.TextBox2Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(25, 213);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(289, 23);
+			this.progressBar1.TabIndex = 7;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(75, 133);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(76, 42);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Качаем!";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(25, 88);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(80, 23);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Куда качаем";
+			// 
+			// progressLabel
+			// 
+			this.progressLabel.Location = new System.Drawing.Point(120, 187);
+			this.progressLabel.Name = "progressLabel";
+			this.progressLabel.Size = new System.Drawing.Size(100, 23);
+			this.progressLabel.TabIndex = 10;
+			// 
+			// TokenBox
+			// 
+			this.TokenBox.Location = new System.Drawing.Point(120, 49);
+			this.TokenBox.Name = "TokenBox";
+			this.TokenBox.Size = new System.Drawing.Size(106, 20);
+			this.TokenBox.TabIndex = 11;
+			// 
+			// GetTokenButton
+			// 
+			this.GetTokenButton.Location = new System.Drawing.Point(200, 133);
+			this.GetTokenButton.Name = "GetTokenButton";
+			this.GetTokenButton.Size = new System.Drawing.Size(75, 42);
+			this.GetTokenButton.TabIndex = 12;
+			this.GetTokenButton.Text = "Получить токен";
+			this.GetTokenButton.UseVisualStyleBackColor = true;
+			this.GetTokenButton.Click += new System.EventHandler(this.GetTokenButtonClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(363, 262);
+			this.Controls.Add(this.GetTokenButton);
+			this.Controls.Add(this.TokenBox);
+			this.Controls.Add(this.progressLabel);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.PathBox);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "MainForm";
+			this.Text = "Пакетная скачка музыки из Вконтакте";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		private System.Windows.Forms.Button GetTokenButton;
+		private System.Windows.Forms.Label progressLabel;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.TextBox PathBox;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox TokenBox;
+	}
+}
---
+++ musicvk/musicvk/MainForm.cs
@@ -0,0 +1,156 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Никитка
+ * Date: 30.07.2016
+ * Time: 13:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace musicvk
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public int AudioCount =0;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+				
+		
+		void TextBox2Click(object sender, EventArgs e)
+		{
+			folderBrowserDialog1 = new FolderBrowserDialog();
+			folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+			folderBrowserDialog1.ShowDialog();
+			PathBox.Text = folderBrowserDialog1.SelectedPath;
+		}
+		
+		void GetTokenButtonClick(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("https://oauth.vk.com/authorize?client_id=5568434&redirect_uri=https://oauth.vk.com/blank.html&scope=audio&response_type=token&v=5.53");
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			string Token = TokenBox.Text.Substring(45,85);
+			//Лютый пиздец отсюда
+			string UID = TokenBox.Text.Substring(156);
+			string SongFolder = PathBox.Text;
+			string deb = GET("https://api.vk.com/method/audio.get","owner_id="+UID+"&access_token="+Token+"&v=5.53");
+			Dictionary<string,Dictionary<string,object>> JSON = JsonConvert.DeserializeObject<Dictionary<string,Dictionary<string,object>>>(GET("https://api.vk.com/method/audio.get","owner_id="+UID+"&access_token="+Token+"&v=5.53"));
+			//И досюда. Не лезть!
+			AudioCount = Convert.ToInt32(JSON["response"]["count"]);
+			List<Dictionary<string,string>> Songs = Songlist(((JArray)(JSON["response"]["items"])).ToObject<Dictionary<string,string>[]>());
+			progressBar1.Minimum=0;
+			progressBar1.Maximum=AudioCount;
+			progressBar1.Value =0;
+			progressBar1.Step = 1;
+			
+			
+			Thread thread = new Thread(() =>DoWork(progressBar1,AudioCount,Songs,SongFolder));
+			thread.Start();
+		}
+			
+		
+		public List<Dictionary<string,string>> Songlist (Dictionary<string,string>[] json)
+		{
+			List<Dictionary<string,string>> response = new List<Dictionary<string,string>>();
+			foreach (Dictionary<string,string> song in json)
+			{
+				Dictionary<string,string> addon = new Dictionary<string, string>();
+				addon.Add ("name",song["artist"]+" — "+song["title"]);
+				addon.Add ("url",song["url"]);
+				response.Add(addon);
+			}
+			return response;
+		}
+		private static string GET(string Url, string Data)
+		{
+			System.Net.WebRequest req = System.Net.WebRequest.Create(Url + "?" + Data);
+			System.Net.WebResponse resp = req.GetResponse();
+			System.IO.Stream stream = resp.GetResponseStream();
+			System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+			string Out = sr.ReadToEnd();
+			sr.Close();
+			return Out;
+		}
+		
+		
+		public void DoWork(ProgressBar progressBar1, int AudioCount, List<Dictionary<string,string>> Songs, string SongFolder)
+    {
+        while (!_shouldStop)
+        {
+            
+			int i = 1;
+			foreach (Dictionary<string,string> Song in Songs)
+			{
+				
+				using (var client = new WebClient())
+					{
+					try
+					{
+					client.DownloadFile(Song["url"], SongFolder+"/"+Song["name"]+".mp3");
+					}
+					catch (WebException e )
+					{
+						if (e.Status == WebExceptionStatus.Timeout)
+						{
+							Console.WriteLine(e.Status.ToString());
+							for (int a = 0; a<=5;a++)
+							{
+								client.DownloadFile(Song["url"], SongFolder+"/"+Song["name"]+".mp3");
+							}
+						}
+					}
+					}
+				Invoke((MethodInvoker)delegate
+    	  		          {
+                                progressLabel.Text = i+"/"+AudioCount;
+								i++;
+								progressBar1.PerformStep();
+				       });
+				Thread.Sleep(1);
+				if (i==AudioCount)
+				{
+					_shouldStop=true;
+				}
+				
+        }
+    }
+        return;
+    }
+		
+		    public void RequestStop()
+    {
+        _shouldStop = true;
+    }
+    // Volatile is used as hint to the compiler that this data
+    // member will be accessed by multiple threads.
+    private volatile bool _shouldStop;
+		
+
+	
+    
+}
+	
+}
